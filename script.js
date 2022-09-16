51209d0a9b1f3e4430d9cf2d416e4f50ff6c22ed
natibi121 <natibi279@gmail.com>
2022-09-16 08:57:03 +0200

    folytatás1

--- script.js
+++ script.js
@@ -1,18 +1,63 @@
-function ell() {
-    if(document.getElementById('psw').value != document.getElementById('psw2').value){
-        alert('Nem ugyan az a jelszo');
-        document.getElementById('psw2').value = "";
+function ell(){
+    hiba = false;
+    pattern = /[0-9]/;
+    password = document.getElementById("psw").value;
+    if (password == "") {
+        alert("Töltse ki a jelszó mezőt!");
+        return
+    }
+    result = password.match(pattern);
+    if (!result){
+        alert("Számjegyet is tartalmazzon a jelszó!");
+        document.getElementById("psw").value = "";
+        return;
+    }
+    pattern = /[a-z]/;
+    result = password.match(pattern);
+    if (!result){
+        hiba = true;
+        alert("Kisbetűt is tartalmazzon a jelszó!");
+        document.getElementById("psw").value = "";
+        return;
+    }
+    pattern = /[A-Z]/;
+    result = password.match(pattern);
+    if (!result){
+        hiba = true;
+        alert("Nagybetűt is tartalmazzon a jelszó!");
+        document.getElementById("psw").value = "";
+        return;
+    }
+    pattern = /[@_$]/;
+    result = password.match(pattern);
+    if (!result){
+        hiba = true;
+        alert("Különleges karaktert is tartalmazzon a jelszó!");
+        document.getElementById("psw").value = "";
+        return;
     }
 }
 
-/*
-function m_t() {
-    let input = document.getElementsByTagName('input');
-    for(let i = 0; i < input.length; i++){
-        if (input[i].type === 'radio' || input[i].type === 'checkbox'){
-            input[i].checked = false;
-        } else {
-            input[i].value = '';
+function ell2() {
+
+    password = document.getElementById("psw").value;
+    password2 = document.getElementById("psw2").value;
+    if (password2 != ''){
+
+        if (password != password2){
+            alert("Nem egyezik a két jelszó!");
+            document.getElementById("psw2").focus();
+            document.getElementById("psw2").value = "";
         }
     }
-}*/
+}
+
+function mezottorol(){
+    mezok = document.getElementsByTagName("input");
+    for (let index = 0; index < mezok.length; index++) {
+        if (mezok[index].type == "radio" || mezok[index].type == "checkbox"){ 
+            mezok[index].checked = false;
+        }
+        mezok[index]. value = "";
+    }
+}
